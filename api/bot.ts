d1de46d07a924d51d75593c14efae00e37632d15
Timothy Wu <timothywu.mail@gmail.com>
2024-02-18 20:12:25 -0800

    Bot API

---
+++ api/bot.ts
@@ -0,0 +1,25 @@
+import { InteractionType, verifyKey } from "discord-interactions";
+import { VercelRequest, VercelResponse } from "@vercel/node";
+
+const BOT_PUBLIC_KEY = process.env.DISCORD_BOT_PUBLIC_KEY as string;
+
+export default async function handler(
+  request: VercelRequest,
+  response: VercelResponse
+) {
+  const signature = request.headers["x-signature-ed25519"] as string;
+  const timestamp = request.headers["x-signature-timestamp"] as string;
+  const rawBody = JSON.stringify(request.body);
+
+  if (!verifyKey(rawBody, signature, timestamp, BOT_PUBLIC_KEY)) {
+    return response.status(401).end("Invalid request.");
+  }
+
+  switch (request.body["type"]) {
+    case InteractionType.PING:
+      console.log("Discord Ping");
+      return response.status(200).json({ type: InteractionType.PING });
+    default:
+      return response.status(404).end("Unknown type");
+  }
+}
