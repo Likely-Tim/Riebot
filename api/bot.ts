4c6fb7602265888b9b47cc3fd23821efda37014f
Timothy Wu <timothywu.mail@gmail.com>
2024-02-18 20:40:13 -0800

    TEST

--- api/bot.ts
+++ api/bot.ts
@@ -3,23 +3,16 @@ import { VercelRequest, VercelResponse } from "@vercel/node";
 
 const BOT_PUBLIC_KEY = process.env.DISCORD_BOT_PUBLIC_KEY as string;
 
-async function verifyRequest(request: VercelRequest) {
-  if (request.method === "POST") return false;
-
-  console.log(request);
-  const signature = request.headers["x-signature-ed25519"] as string;
-  const timestamp = request.headers["x-signature-timestamp"] as string;
-  const rawBody = JSON.stringify(request.body);
-
-  return verifyKey(rawBody, signature, timestamp, BOT_PUBLIC_KEY);
-}
-
 export default async function handler(
   request: VercelRequest,
   response: VercelResponse
 ) {
   console.log(request);
-  if (!(await verifyRequest(request))) {
+  const signature = request.headers["x-signature-ed25519"] as string;
+  const timestamp = request.headers["x-signature-timestamp"] as string;
+  const rawBody = JSON.stringify(request.body);
+
+  if (!verifyKey(rawBody, signature, timestamp, BOT_PUBLIC_KEY)) {
     return response.status(401).end("Invalid request.");
   }
 
